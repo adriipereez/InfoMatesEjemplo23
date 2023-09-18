f131b8b12bf6d5c5a12baec41c99372303f9ab55
adriipereez <adriaperez@ceroca.cat>
2023-09-18 18:45:08 +0200

    BORDES

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -19,10 +19,24 @@ public class NauJugador : MonoBehaviour
         float direccioVertical = Input.GetAxisRaw("Vertical");
         //Debug.Log(direccioHoritzontal);
 
-        Vector2 dreccioIndicada = new Vector2(direccioHoritzontal, direccioVertical);
+        Vector2 dreccioIndicada = new Vector2(direccioHoritzontal, direccioVertical).normalized;
+        SpriteRenderer SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        float anchura = SpriteRenderer.bounds.size.x / 2;
+        float altura = SpriteRenderer.bounds.size.y / 2;
+
+        float limitEsquerraX = -Camera.main.orthographicSize * Camera.main.aspect + anchura;
+        float limitDretaX = Camera.main.orthographicSize * Camera.main.aspect - anchura;
+
+        float limitArribaY = Camera.main.orthographicSize - altura;
+        float limitAbajoY = -Camera.main.orthographicSize + altura;
 
         Vector2 novaPos = transform.position; //retorna posicio actual de la nau.
         novaPos += dreccioIndicada * _velNau * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, limitEsquerraX, limitDretaX);
+        novaPos.y = Mathf.Clamp(novaPos.y, limitAbajoY, limitArribaY);
+
         transform.position = novaPos;
     }
 }
